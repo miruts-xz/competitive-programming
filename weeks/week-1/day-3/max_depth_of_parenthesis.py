f3d6ec68856d9342ef1b41ba651910a5f0ac258b
miruts <miruts.hadush@aait.edu.et>
2021-01-06 10:43:13 +0300

    implemented max_depth_of_parenthesis.py

---
+++ weeks/week-1/day-3/max_depth_of_parenthesis.py
@@ -0,0 +1,22 @@
+#  Copyright (c) 2021. This code is licensed to mire
+#  Copying and / or distributing without appropriate permission from author is
+#  illegal and would mount to theft of intellectual resource.
+#  Please contact developer at miruts.hadush@aait.edu.et prior to
+#  copying/distributing to ask and get proper authorizations. Don't get too scared I'm kidding :)
+
+# Method implements maximum depth of parenthesis leetcode challenge
+# @https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses:
+def max_depth(s: str) -> int:
+    m = 0
+    stack = []
+    temp_max = 0
+    for c in s:
+        if c == '(':
+            temp_max += 1
+            m = max(m, temp_max)
+        elif c == ")":
+            temp_max -= 1
+    return m
+
+
+print(max_depth("(1+(2*3)+((8)/4))+1"))
