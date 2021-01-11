d3998d0b796cc91926876fcae1e8d0e191c689a8
miruts <miruts.hadush@aait.edu.et>
2021-01-11 15:57:11 +0300

    solve delete_node_in_linked_list.py

--- weeks/week-2/day-7/delete_node_in_linked_list.py
+++ weeks/week-2/day-7/delete_node_in_linked_list.py
@@ -1,37 +1,12 @@
-#  Copyright (c) 2021. This code is licensed to mire
-#  Copying and / or distributing without appropriate permission from author is
-#  illegal and would mount to theft.
-#  Please contact developer at miruts.hadush@aait.edu.et prior to
-#  copying/distributing to ask and get proper authorizations.
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
-from design_linked_list import ListNode
 
-# Code implements deletion of Node from linked list
-# create linked list
-head = ListNode(1)
-node2 = ListNode(2)
-node3 = ListNode(3)
-node4 = ListNode(4)
-
-head.next = node2
-node2.next = node3
-node3.next = node4
-tail = node4
-
-# delete node with value 3
-node = head
-while node:
-    if node.val == 3 and node == head:
-        head = head.next
-        break
-    if node.next.val == 3:
+class Solution:
+    def deleteNode(self, node):
         next_node = node.next
+        node.val = next_node.val
         node.next = next_node.next
-        break
-    node = node.next
-
-# After deletion
-_node = head
-while _node:
-    print(_node.val)
-    _node = _node.next
