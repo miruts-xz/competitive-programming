e5035498b0b184374278886b9969744b43d1082f
miruts <miruts.hadush@aait.edu.et>
2021-01-12 22:25:13 +0300

    solve min_stack.py

---
+++ weeks/week-2/day-7/min_stack.py
@@ -0,0 +1,27 @@
+#  Copyright (c) 2021. This code is licensed to mire
+#  Copying and / or distributing without appropriate permission from author is
+#  illegal and would mount to theft.
+#  Please contact developer at miruts.hadush@aait.edu.et prior to
+#  copying/distributing to ask and get proper authorizations.
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        val = self.stack.pop()
+        self.push(val)
+        return val
+
+    def getMin(self) -> int:
+        return min(self.stack)
