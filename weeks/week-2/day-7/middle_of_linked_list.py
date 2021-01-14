69319d8d021e6578e476884f48d4a12b970bf20d
miruts <miruts.hadush@aait.edu.et>
2021-01-14 22:21:25 +0300

    improve middle_of_linked_list.py

--- weeks/week-2/day-7/middle_of_linked_list.py
+++ weeks/week-2/day-7/middle_of_linked_list.py
@@ -1,9 +1,3 @@
-#  Copyright (c) 2021. This code is licensed to mire
-#  Copying and / or distributing without appropriate permission from author is
-#  illegal and would mount to theft.
-#  Please contact developer at miruts.hadush@aait.edu.et prior to
-#  copying/distributing to ask and get proper authorizations.
-
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -11,14 +5,13 @@ class ListNode:
         self.next = next
 
 
-def middle_node(head: ListNode) -> ListNode:
-    count = 0
-    node = head
-    while node:
-        count += 1
-        node = node.next
-    node_number = count // 2
-    node = head
-    for i in range(node_number):
-        node = node.next
-    return node
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        s, f = head, head
+        while f:
+            if f.next is None:
+                return s
+            s = s.next
+            fnx = f.next
+            f = fnx.next
+        return s
