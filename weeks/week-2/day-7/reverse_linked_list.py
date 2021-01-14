341ffb56b56430e2b53eca153ce18264967f816d
miruts <miruts.hadush@aait.edu.et>
2021-01-14 22:25:04 +0300

    solve reverse_linked_list.py recursively

--- weeks/week-2/day-7/reverse_linked_list.py
+++ weeks/week-2/day-7/reverse_linked_list.py
@@ -1,53 +1,22 @@
-#  Copyright (c) 2021. This code is licensed to mire
-#  Copying and / or distributing without appropriate permission from author is
-#  illegal and would mount to theft.
-#  Please contact developer at miruts.hadush@aait.edu.et prior to
-#  copying/distributing to ask and get proper authorizations.
-from design_linked_list import ListNode
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
-# Method implements reversing linked list
-def reverse_linked_list(head: ListNode) -> ListNode:
-    node_list = []
-    node = head
-    while node:
-        node_list.append(ListNode(node.val))
-        node = node.next
-    local_head = None
-    for i in range(len(node_list) - 1, 0, -1):
-        if i == len(node_list) - 1:
-            local_head = node_list[i]
-            local_head.next = node_list[i - 1]
-        else:
-            node = node_list[i]
-            node.next = node_list[i - 1]
-    return local_head
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head:
+            return head
+        self.reverse(head, head.next)
+        return self.head
 
-
-# Create linked list
-head = ListNode()
-node1 = ListNode(1)
-node2 = ListNode(2)
-node3 = ListNode(3)
-node4 = ListNode(4)
-
-head.next = node1
-node1.next = node2
-node2.next = node3
-node3.next = node4
-
-node = head
-# Before reversal
-if __name__ == '__main__':
-    print('Before')
-    while node:
-        print(node.val)
-        node = node.next
-
-node = reverse_linked_list(head)
-# After reversal
-if __name__ == '__main__':
-    print('After')
-    while node:
-        print(node.val)
-        node = node.next
+    def reverse(self, curr, nxt):
+        if nxt is None:
+            self.head = curr
+            return curr
+        prev_head = self.reverse(curr.next, nxt.next)
+        curr.next = None
+        prev_head.next = curr
+        return curr
