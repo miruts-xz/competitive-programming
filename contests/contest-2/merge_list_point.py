d82fcb4d93a566a12614f4ba5a1abd8b2ae315df
miruts <53303150+miruts@users.noreply.github.com>
2021-01-16 16:58:16 +0300

    Update merge_list_point.py

--- contests/contest-2/merge_list_point.py
+++ contests/contest-2/merge_list_point.py
@@ -1,10 +1,12 @@
 def findMergeNode(head1, head2):
     h1_ptr = head1
-    h2_ptr = head2
+    st = set()
     while h1_ptr:
-        while h2_ptr:
-            if hash(h1_ptr) == hash(h2_ptr):
-                return h1_ptr.data
-            h2_ptr = h2_ptr.next
-        h2_ptr = head2
-        h1_ptr = h1_ptr.next
+        st.add(h1_ptr)
+    while head2:
+        l1 = len(st)
+        st.add(head2)
+        l2 = len(st)
+        if l1 == l2:
+            return head2.data
+        head2 = head2.next
