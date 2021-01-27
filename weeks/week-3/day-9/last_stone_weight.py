7a46bcc36a0b7d64872fc1e02f98e80fd41d7806
miruts <miruts.hadush@aait.edu.et>
2021-01-27 21:59:19 +0300

    solve last_stone_weight.py

---
+++ weeks/week-3/day-9/last_stone_weight.py
@@ -0,0 +1,14 @@
+import heapq
+
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        for i in range(len(stones)):
+            stones[i] *= -1
+        heapq.heapify(stones)
+        while len(stones) > 1:
+            y = -heapq.heappop(stones)
+            x = -heapq.heappop(stones)
+            if x != y:s
+                heapq.heappush(stones, x - y)
+        return -stones[0] if stones else 0
