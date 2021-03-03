0cdea989732a40fc5a973073ddfcce6311d952a5
miruts <miruts.hadush@aait.edu.et>
2021-03-03 16:20:15 +0300

    solve container_with_most_water.py

---
+++ weeks/week-7/container_with_most_water.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        mx = 0
+        left = 0
+        right = len(height) - 1
+
+        while left < right:
+            mx = max((min(height[left], height[right]) * (right - left)), mx)
+            if height[left] <= height[right]:
+                left += 1
+            else:
+                right -= 1
+        return mx
