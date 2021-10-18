152103534024c294300c589a981da635769a708d
miruts <miruts.hadush@aait.edu.et>
2021-10-18 10:23:01 +0300

    Solve Path sum III

---
+++ camp-questions/437.path-sum-iii.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode id=437 lang=python3
+#
+# [437] Path Sum III
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+from typing import DefaultDict, Optional
+
+
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        ans = 0
+        lookup = DefaultDict(int)
+        def _pathSum(root, rem):
+            nonlocal ans
+            if not root:
+                return
+            if root.val == rem:
+                ans += 1
+            ans += lookup[targetSum+rem-root.val]
+            lookup[rem-root.val] += 1
+            _pathSum(root.left, rem-root.val)
+            _pathSum(root.right,rem-root.val)
+            lookup[rem-root.val] -= 1
+        _pathSum(root,targetSum)
+        return ans
+    
+# @lc code=end
+
