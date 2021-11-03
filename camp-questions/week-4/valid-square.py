f86775c6b32b702f4d7a16297c83070a0944d196
miruts <miruts.hadush@aait.edu.et>
2021-11-03 21:59:23 +0300

    Solve valid-square.py

--- camp-questions/week-4/valid-square.py
+++ camp-questions/week-4/valid-square.py
@@ -9,7 +9,6 @@ class Solution:
             top3 = get_dist(p1, p3)
             top4 = get_dist(p1, p4)
             if 0 in [top2, top3, top4]: return False
-            valid = False
             if top2 == top3:
                 return abs(top4 - math.sqrt(top2**2+top3**2)) < 10**-5
             if top2 == top4:
